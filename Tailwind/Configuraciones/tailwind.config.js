f378a5210915983f92e2110abaace05be9f1c3c5
nikoDev <nikolas090189@gmail.com>
2023-09-25 20:12:17 -0300

    Personalizacion de tailwind

---
+++ Tailwind/Configuraciones/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // screens: {
+    //   mediano: "500px",
+    // },
+    extend: {
+      screens: {
+        mediano: "500px",
+      },
+    },
+  },
+  plugins: [],
+};
